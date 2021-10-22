69348c50484877e92890319c9f3c3a6abb56f2cc
AyrielNoriega <anoriega2310@gmail.com>
2021-10-22 13:00:40 -0500

    creacion clase user

--- includes/user.php
+++ includes/user.php
@@ -20,6 +20,21 @@ class User extends DB{
         }
     }
 
+    public function setUser( $user ){
+        $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE username = :user ');
+        $query->execute(['user' => $user]);
+
+        foreach ($query as $currentUser ) {   
+            $this->nombre = $currentUser['nombre'];
+            $this->username = $currentUser['username'];
+        }
+
+        public function getNombre(){
+            return this->nombre;
+        }
+
+    }
+
 }
 
 ?>
