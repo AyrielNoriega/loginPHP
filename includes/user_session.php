b24f9742ca289965a651ffae04995a844c99f854
AyrielNoriega <anoriega2310@gmail.com>
2021-10-22 13:07:21 -0500

    creacion clase userSession

--- includes/user_session.php
+++ includes/user_session.php
@@ -1,5 +1,28 @@
 <?php
 
+//ambiente de sesiones
+
+class UserSession{
+
+    public function __construct(){
+        session_start();
+    }
+
+    public function setCurrentUser( $user   ){
+        $_SESSION['user'] = $user;
+    }
+
+    public function getCurrentUser(){
+        return $_SESSION['user'];
+    }
+
+    public function closeSession(){
+        session_unset(); //destruir valores de sesion
+        session_destroy(); //destruir sesion
+    }
+
+
+}
 
 
 ?>
