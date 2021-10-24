45878ec588c763a435d9b9d028245f452f1cbb36
AyrielNoriega <anoriega2310@gmail.com>
2021-10-24 18:37:05 -0500

    Config view new user

---
+++ vistas/create_user.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sessiones</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 mx-auto">
+
+            <form  action="" method="post">
+                    <h2>Registro de usuario</h2>
+                    <?php
+                        if ( isset( $errorLogin ) ) {
+                            echo $errorLogin; 
+                        }
+
+                    ?>
+                    <div class="form-group">
+                        <label for="username" >Nombre de usuario</label>
+                        <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+                        <small id="e    mailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+    
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+                <a href="/vistas/login.php">Regresar</a> 
+            </div>
+        </div>
+    </div>
+
+
+    
+</body>
+</html>
